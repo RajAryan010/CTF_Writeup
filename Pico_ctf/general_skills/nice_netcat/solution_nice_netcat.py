13f024a860e2ace7dd68fb216cae277ec89cc8a4
RajAryan010 <115923830+RajAryan010@users.noreply.github.com>
2022-12-09 10:46:40 +0530

    Add files via upload

---
+++ Pico_ctf/general_skills/nice_netcat/solution_nice_netcat.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python3
+
+ascii_text = chr(112),chr(105),chr(99),chr(111),chr(67),chr(84),chr(70),chr(123),chr(103),chr(48),chr(48),chr(100),chr(95),chr(107),chr(49),chr(116),chr(116),chr(121),chr(33),chr(95),chr(110),chr(49),chr(99),chr(51),chr(95),chr(107),chr(49),chr(116),chr(116),chr(121),chr(33),chr(95),chr(55),chr(99),chr(48),chr(56),chr(50),chr(49),chr(102),chr(53),chr(125),chr(10)
+#print(ascii_text)
+flag_str_format = "".join(ascii_text)
+
+print(flag_str_format)
